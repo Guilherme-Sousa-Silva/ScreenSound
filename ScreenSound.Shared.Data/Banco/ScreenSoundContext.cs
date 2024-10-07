4b812278b34e8f8a09ae10f639b249ba50958f8c
Bowie <marketing@letscode.com.br>
2024-10-07 15:10:28 -0300

    realizando as configurações necessárias para subir o BD no azure

--- ScreenSound.Shared.Data/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Data/Banco/ScreenSoundContext.cs
@@ -6,7 +6,8 @@ namespace ScreenSound.Banco
 {
     public class ScreenSoundContext : DbContext
     {
-        private readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV1;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        //private readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV1;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        private readonly string connectionString = "Server=tcp:screensoundserver-v0.database.windows.net,1433;Initial Catalog=ScreenSoundV0;Persist Security Info=False;User ID=guilherme;Password=Gui@24254749;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public DbSet<Artista> Artistas { get; set; }
         public DbSet<Musica> Musicas { get; set; }
